f17499cc858944e83bab78d9d319715f6f4e9dcb
tywr <tanguy.viv@gmail.com>
2026-04-02 20:18:05 +0200

    add: uppercase A

--- src/glyphs/uppercase/a.py
+++ src/glyphs/uppercase/a.py
@@ -1,7 +1,7 @@
+from math import atan, sin, cos
 from config import FontConfig as fc
 from glyph import Glyph
-from shapes.superellipse_arch import draw_superellipse_arch
-from shapes.corner import draw_corner
+from shapes.polygon import draw_polygon
 from shapes.rect import draw_rect
 
 
@@ -15,17 +15,42 @@ class UppercaseAGlyph(Glyph):
         stroke: int,
     ):
         offset = 0
-        width = fc.body_width + fc.h_overshoot
-        loop_ratio = fc.a_ratio
-        hx = fc.a_hx
-        hy = fc.a_hy
-        cap_hx = 200
-        cap_hy = 200
-        len_cap = 235
+        width = 420
+        bar_height = 320
 
         x1 = fc.width / 2 - width / 2 - stroke / 2 + offset
-        y1 = 0
         x2 = fc.width / 2 + width / 2 + stroke / 2 + offset
-        y2 = fc.ascent
         xmid = x1 + (x2 - x1) / 2
 
+        a = width / 2
+        b = fc.ascent
+        theta = atan(b / a)
+        x_delta = stroke / sin(theta)
+
+        draw_polygon(
+            pen,
+            points=[
+                (xmid + x_delta / 2, fc.ascent),
+                (xmid + width / 2 + x_delta / 2, 0),
+                (xmid + width / 2 - x_delta / 2, 0),
+                (xmid - x_delta / 2, fc.ascent),
+            ],
+        )
+        draw_polygon(
+            pen,
+            points=[
+                (xmid + x_delta / 2, fc.ascent),
+                (xmid - width / 2 + x_delta / 2, 0),
+                (xmid - width / 2 - x_delta / 2, 0),
+                (xmid - x_delta / 2, fc.ascent),
+            ],
+        )
+        draw_polygon(
+            pen,
+            points=[
+                (xmid + width / 2 - bar_height * cos(theta), bar_height * sin(theta)),
+                (xmid + width / 2 - (bar_height - stroke) * cos(theta), (bar_height - stroke) * sin(theta)),
+                (xmid - width / 2 + (bar_height - stroke) * cos(theta), (bar_height - stroke) * sin(theta)),
+                (xmid - width / 2 + bar_height * cos(theta), bar_height * sin(theta)),
+            ],
+        )
