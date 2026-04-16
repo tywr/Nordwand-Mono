604200e496951bbdcff8d889880e0a3912ca2afd
tywr <tanguy.viv@gmail.com>
2026-04-16 23:58:23 +0200

    add: top-left for square corner

--- src/draw/square_corner.py
+++ src/draw/square_corner.py
@@ -59,7 +59,21 @@ def draw_square_corner(
         pen.closePath()
 
     elif orientation == "top-left":
-        ...
+        ix1 = x1 - stroke_x
+        iy1 = y1
+        ix2 = x2
+        iy2 = y2 - stroke_y
+        xc1, yc1 = x1, y2 - stroke_y - gy
+        xc2, yc2 = x1 - stroke_x - gx, y2
+
+        pen.moveTo((x1, y1))
+        pen.lineTo((xc1, yc1))
+        pen.curveTo((xc1, yc1 + stroke_y), (xc2 + stroke_x, yc2), (xc2, yc2))
+        pen.lineTo((x2, y2))
+        pen.lineTo((ix2, iy2))
+        pen.lineTo((ix1, iy2))
+        pen.lineTo((ix1, iy1))
+        pen.closePath()
 
     elif orientation == "top-right":
         ix1 = x1 + stroke_x
