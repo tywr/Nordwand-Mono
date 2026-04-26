76718f1d7e1b6e7c690ec11d699b85b48a5f5fe1
tywr <tanguy.viv@gmail.com>
2026-04-26 11:51:16 +0200

    adjust: thicker asterisk

--- src/glyphs/special/asterisk.py
+++ src/glyphs/special/asterisk.py
@@ -7,32 +7,34 @@ class AsteriskGlyph(Glyph):
     unicode = "0x2A"
     offset = 0
     width_ratio = 1.31
+    stroke_ratio = 1.2
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
             offset=self.offset, height="x_height", width_ratio=self.width_ratio
         )
+        s = dc.stroke_alt * self.stroke_ratio
         ymid = dc.math
         draw_rect(
             pen,
             b.x1,
-            ymid - dc.stroke_alt / 2,
+            ymid - s / 2,
             b.x2,
-            ymid + dc.stroke_alt / 2,
+            ymid + s / 2,
             rotate=30,
         )
         draw_rect(
             pen,
             b.x1,
-            ymid - dc.stroke_alt / 2,
+            ymid - s / 2,
             b.x2,
-            ymid + dc.stroke_alt / 2,
+            ymid + s / 2,
             rotate=-30,
         )
         draw_rect(
             pen,
-            b.xmid - dc.stroke_alt / 2,
+            b.xmid - s / 2,
             ymid - b.width / 2,
-            b.xmid + dc.stroke_alt / 2,
+            b.xmid + s / 2,
             ymid + b.width / 2,
         )
