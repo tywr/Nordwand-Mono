1270db877eda0890251a25a2ca213efec990a600
tywr <tanguy.viv@gmail.com>
2026-05-06 11:21:50 +0200

    add: traditional w lowercase

--- src/glyphs/lowercase/w.py
+++ src/glyphs/lowercase/w.py
@@ -1,109 +1,54 @@
-from math import cos, sin
-import ufoLib2
-from booleanOperations.booleanGlyph import BooleanGlyph
 from glyphs import Glyph
-from draw.rect import draw_rect
-from draw.parallelogramm import draw_parallelogramm_vertical
-from draw.polygon import draw_polygon
+from draw.parallelogramm import draw_parallelogramm
 
 
 class LowercaseWGlyph(Glyph):
     name = "lowercase_w"
     unicode = "0x77"
     offset = 0
-    overlap = 0.65
-    overlap_middle = 0.5
-    depth = 0.6
-    inner_thickness_ratio = 1.5
-    inner_height = 0.4
-    width_ratio = 1.1
-    ink_trap_height = 0.75
+    outer_branch_ratio = 0.35
+    inner_height = 1
+    width_ratio = 1.26
+    stroke_ratio = 0.88
+    inner_stroke_ratio = 0.74
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
             offset=self.offset,
             width_ratio=self.width_ratio,
-            min_margin=dc.min_margin_lowercase,
+            # min_margin=dc.min_margin_lowercase,
         )
-        sx = dc.stroke_x
-        delta = self.inner_thickness_ratio * dc.stroke_x
-        yi = b.y2 - self.inner_height * b.height
-        yik = b.y2 - self.ink_trap_height * b.height
+        sx = dc.stroke_x * self.stroke_ratio
+        isx = dc.stroke_x * self.inner_stroke_ratio
+        xi1 = b.x1 + self.outer_branch_ratio * b.width
+        xi2 = b.x2 - self.outer_branch_ratio * b.width
+        yi = b.y1 + self.inner_height * b.height
 
-        glyph = ufoLib2.objects.Glyph()
-        gpen = glyph.getPen()
-
-        # Vertical stems
-        draw_rect(gpen, b.x1, b.y1, b.x1 + sx, b.y2)
-        draw_rect(gpen, b.x2 - sx, b.y1, b.x2, b.y2)
-
-        # Middle branches
-        draw_parallelogramm_vertical(
-            gpen,
+        draw_parallelogramm(
+            pen, 0, 0, b.x1, b.y2, xi1, b.y1, delta=sx, direction="bottom-right"
+        )
+        draw_parallelogramm(
+            pen, 0, 0, b.x2, b.y2, xi2, b.y1, delta=sx, direction="bottom-left"
+        )
+        draw_parallelogramm(
+            pen,
             0,
             0,
-            b.x1 + dc.stroke_x,
+            xi1 - isx,
             b.y1,
-            b.xmid,
-            yi + delta / 2,
-            direction="top-right",
-            delta=delta,
-        )
-        theta, _ = draw_parallelogramm_vertical(
-            gpen,
-            0,
-            0,
-            b.x2 - dc.stroke_x,
-            b.y1,
-            b.xmid,
-            yi + delta / 2,
-            direction="top-left",
-            delta=delta,
-        )
-
-        cut_glyph = ufoLib2.objects.Glyph()
-
-        # Cut the upper part, where the branches meet
-        draw_rect(
-            cut_glyph.getPen(),
-            b.x1 + sx,
+            b.xmid + isx / 2,
             yi,
-            b.x2 - sx,
-            yi + delta / 2,
+            delta=isx,
+            direction="top-right",
         )
-
-        le = max(0, (b.y1 + delta - yik) / cos(theta))
-        if le > 0:
-            draw_polygon(
-                cut_glyph.getPen(),
-                points=[
-                    (b.x2 - sx, b.y1 + delta),
-                    (
-                        b.x2 - sx + le * sin(theta),
-                        b.y1 + delta - le * cos(theta),
-                    ),
-                    (
-                        b.x2 - sx + le * sin(theta),
-                        b.y1 + delta - le * cos(theta),
-                    ),
-                    (b.x2 - sx, (b.y1 + b.y2 + delta) / 2),
-                ],
-            )
-            draw_polygon(
-                cut_glyph.getPen(),
-                points=[
-                    (b.x1 + sx, b.y1 + delta),
-                    (
-                        b.x1 + sx - le * sin(theta),
-                        b.y1 + delta - le * cos(theta),
-                    ),
-                    (
-                        b.x1 + sx - le * sin(theta),
-                        b.y1 + delta - le * cos(theta),
-                    ),
-                    (b.x1 + sx, (b.y1 + b.y2 + delta) / 2),
-                ],
-            )
-
-        res = BooleanGlyph(glyph).difference(BooleanGlyph(cut_glyph))
-        res.draw(pen)
+        draw_parallelogramm(
+            pen,
+            0,
+            0,
+            xi2 + isx,
+            b.y1,
+            b.xmid - isx / 2,
+            yi,
+            delta=isx,
+            direction="top-left",
+        )
