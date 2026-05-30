f7e1177e8b3074d044b264ff3818163bd4141ae9
tywr <tanguy.viv@gmail.com>
2026-05-30 17:51:11 +0200

    fix: issue with square corner when corner is too short

--- src/draw/square_corner.py
+++ src/draw/square_corner.py
@@ -1,3 +1,8 @@
+import ufoLib2
+from booleanOperations.booleanGlyph import BooleanGlyph
+from draw.rect import draw_rect
+
+
 def draw_square_corner(
     pen,
     stroke_x,
@@ -21,50 +26,60 @@ def draw_square_corner(
         top-left:     corner at (x2, y1) — curve goes left then down
         bottom-left:  corner at (x1, y2) — curve goes down then left
     """
-    w = abs(x2 - x1)
-    h = abs(y2 - y1)
     sx, sy = stroke_x, stroke_y
+    ml = max(x2, x1) - min(x2, x1)
 
+    glyph = ufoLib2.objects.Glyph()
+    cut_glyph = ufoLib2.objects.Glyph()
+    gpen = glyph.getPen()
+    cpen = cut_glyph.getPen()
     if orientation == "bottom-left":
-        pen.moveTo((x1, y1))
-        pen.lineTo((x1, y2 + gy))
-        pen.curveTo((x1, y2 + sy / 2), (x1 - sx / 2, y2), (x1 - gx, y2))
-        pen.lineTo((x2, y2))
-        pen.lineTo((x2, y2 + sy))
-        pen.lineTo((x1 - sx - gx, y2 + sy))
-        pen.curveTo((x1 - sx, y2 + sy), (x1 - sx, y2 + sy), (x1 - sx, y2 + sy + gy))
-        pen.lineTo((x1 - sx, y1))
-        pen.closePath()
+        gpen.moveTo((x1, y1))
+        gpen.lineTo((x1, y2 + gy))
+        gpen.curveTo((x1, y2 + sy / 2), (x1 - sx / 2, y2), (x1 - gx, y2))
+        gpen.lineTo((x2, y2))
+        gpen.lineTo((x2, y2 + sy))
+        gpen.lineTo((x1 - sx - gx, y2 + sy))
+        gpen.curveTo((x1 - sx, y2 + sy), (x1 - sx, y2 + sy), (x1 - sx, y2 + sy + gy))
+        gpen.lineTo((x1 - sx, y1))
+        gpen.closePath()
+        draw_rect(cpen, x2 - ml, y1, x2, y2)
 
     elif orientation == "bottom-right":
-        pen.moveTo((x1, y1))
-        pen.lineTo((x1, y2 + gy))
-        pen.curveTo((x1, y2 + sy / 2), (x1 + sx / 2, y2), (x1 + gx, y2))
-        pen.lineTo((x2, y2))
-        pen.lineTo((x2, y2 + sy))
-        pen.lineTo((x1 + sx + gx, y2 + sy))
-        pen.curveTo((x1 + sx, y2 + sy), (x1 + sx, y2 + sy), (x1 + sx, y2 + sy + gy))
-        pen.lineTo((x1 + sx, y1))
-        pen.closePath()
+        gpen.moveTo((x1, y1))
+        gpen.lineTo((x1, y2 + gy))
+        gpen.curveTo((x1, y2 + sy / 2), (x1 + sx / 2, y2), (x1 + gx, y2))
+        gpen.lineTo((x2, y2))
+        gpen.lineTo((x2, y2 + sy))
+        gpen.lineTo((x1 + sx + gx, y2 + sy))
+        gpen.curveTo((x1 + sx, y2 + sy), (x1 + sx, y2 + sy), (x1 + sx, y2 + sy + gy))
+        gpen.lineTo((x1 + sx, y1))
+        gpen.closePath()
+        draw_rect(cpen, x2, y1, x2 + ml, y2)
 
     elif orientation == "top-left":
-        pen.moveTo((x1, y1))
-        pen.lineTo((x1, y2 - gy))
-        pen.curveTo((x1, y2 - sy / 2), (x1 - sx / 2, y2), (x1 - gx, y2))
-        pen.lineTo((x2, y2))
-        pen.lineTo((x2, y2 - sy))
-        pen.lineTo((x1 - sx - gx, y2 - sy))
-        pen.curveTo((x1 - sx, y2 - sy), (x1 - sx, y2 - sy), (x1 - sx, y2 - sy - gy))
-        pen.lineTo((x1 - sx, y1))
-        pen.closePath()
+        gpen.moveTo((x1, y1))
+        gpen.lineTo((x1, y2 - gy))
+        gpen.curveTo((x1, y2 - sy / 2), (x1 - sx / 2, y2), (x1 - gx, y2))
+        gpen.lineTo((x2, y2))
+        gpen.lineTo((x2, y2 - sy))
+        gpen.lineTo((x1 - sx - gx, y2 - sy))
+        gpen.curveTo((x1 - sx, y2 - sy), (x1 - sx, y2 - sy), (x1 - sx, y2 - sy - gy))
+        gpen.lineTo((x1 - sx, y1))
+        gpen.closePath()
+        draw_rect(cpen, x2 - ml, y1, x2, y2)
 
     elif orientation == "top-right":
-        pen.moveTo((x1, y1))
-        pen.lineTo((x1, y2 - gy))
-        pen.curveTo((x1, y2 - sy / 2), (x1 + sx / 2, y2), (x1 + gx, y2))
-        pen.lineTo((x2, y2))
-        pen.lineTo((x2, y2 - sy))
-        pen.lineTo((x1 + sx + gx, y2 - sy))
-        pen.curveTo((x1 + sx, y2 - sy), (x1 + sx, y2 - sy), (x1 + sx, y2 - sy - gy))
-        pen.lineTo((x1 + sx, y1))
-        pen.closePath()
+        gpen.moveTo((x1, y1))
+        gpen.lineTo((x1, y2 - gy))
+        gpen.curveTo((x1, y2 - sy / 2), (x1 + sx / 2, y2), (x1 + gx, y2))
+        gpen.lineTo((x2, y2))
+        gpen.lineTo((x2, y2 - sy))
+        gpen.lineTo((x1 + sx + gx, y2 - sy))
+        gpen.curveTo((x1 + sx, y2 - sy), (x1 + sx, y2 - sy), (x1 + sx, y2 - sy - gy))
+        gpen.lineTo((x1 + sx, y1))
+        gpen.closePath()
+        draw_rect(cpen, x2, y1, x2 + ml, y2)
+
+    res = BooleanGlyph(glyph).difference(BooleanGlyph(cut_glyph))
+    res.draw(pen)
