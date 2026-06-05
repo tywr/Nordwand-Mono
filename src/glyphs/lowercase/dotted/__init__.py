40441fd2ef85733937311b51f49f8b17927d2b07
tywr <tanguy.viv@gmail.com>
2026-06-05 09:41:58 +0200

    adjust: dots on i,j

--- src/glyphs/lowercase/dotted/__init__.py
+++ src/glyphs/lowercase/dotted/__init__.py
@@ -9,6 +9,7 @@ class DottedLowercaseGlyph(Glyph, ABC):
 
     dot_height = 0.25
     dot_position = "xmid"
+    dot_width = 1.1
 
     @abstractmethod
     def draw_base(self, pen, dc): ...
@@ -16,14 +17,15 @@ class DottedLowercaseGlyph(Glyph, ABC):
     def draw(self, pen, dc):
         self.draw_base(pen, dc)
         b = dc.body_bounds(offset=self.offset, width_ratio=self.width_ratio)
+        dw = self.dot_width * dc.stroke_x
 
         # Accent dot
         xpos = getattr(b, self.dot_position)
         dh = self.dot_height * b.height
         draw_rect(
             pen,
-            xpos - dc.stroke_x / 2,
+            xpos - dw / 2,
             dc.accent - dh / 2,
-            xpos + dc.stroke_x / 2,
+            xpos + dw / 2,
             dc.accent + dh / 2,
         )
