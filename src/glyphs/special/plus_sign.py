1da9f8c6e65a6723ecca32cf018fa2a2112e7e2f
tywr <tanguy.viv@gmail.com>
2026-06-21 11:37:28 +0200

    fix: plus sign

--- src/glyphs/special/plus_sign.py
+++ src/glyphs/special/plus_sign.py
@@ -17,12 +17,13 @@ class PlusSignGlyph(Glyph):
         s = dc.stroke_x * self.stroke_ratio
         so = self.side_offset * dc.window_width
         x1, x2 = so, dc.window_width - so
+        w = (x2 - x1)
 
         draw_rect(pen, x1, dc.math - s / 2, x2, dc.math + s / 2)
         draw_rect(
             pen,
             b.xmid - s / 2,
-            dc.math - b.width / 2,
+            dc.math - w / 2,
             b.xmid + s / 2,
-            dc.math + b.width / 2,
+            dc.math + w / 2,
         )
