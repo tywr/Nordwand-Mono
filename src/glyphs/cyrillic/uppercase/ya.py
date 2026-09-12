1c2f50cbdcfd1154f674de42985783684a5fd08e
tywr <tanguy.viv@gmail.com>
2026-09-12 13:04:22 +0200

    add: third layer of cyrillic characters (uppercase)

---
+++ src/glyphs/cyrillic/uppercase/ya.py
@@ -0,0 +1,67 @@
+from glyphs.cyrillic.uppercase import CyrillicUppercaseGlyph
+from draw.loop import draw_loop
+from draw.rect import draw_rect
+from draw.parallelogramm import draw_parallelogramm
+
+
+class CyrillicUppercaseYaGlyph(CyrillicUppercaseGlyph):
+    name = "cyrillic_uppercase_ya"
+    unicode = "0x042F"
+    offset = -20
+    loop_ratio = 0.605
+    loop_width = 0.96
+    branch_start = 0.63
+    width_ratio = 1.05
+
+    def draw(self, pen, dc):
+        b = dc.body_bounds(
+            offset=self.offset,
+            height="cap",
+            overshoot_right=True,
+            width_ratio=self.width_ratio,
+            uppercase=True,
+        )
+        sx, sy = dc.stroke_x * self.stroke_x_ratio, dc.stroke_y * self.stroke_y_ratio
+        hx, hy = b.hx * self.loop_width, b.hy * self.loop_ratio
+        ymid = b.y1 + (1 - self.loop_ratio) * b.height
+        xb = self.branch_start * b.width
+        w = self.loop_width * b.width
+
+        # Left stem
+        draw_rect(pen, b.x2 - sx, 0, b.x2, dc.cap)
+
+        # Upper loop (narrower, displaced left)
+        draw_loop(
+            pen,
+            sx,
+            sy,
+            b.x2 - w,
+            ymid,
+            b.x2,
+            b.y2,
+            hx,
+            hy,
+            cut="right",
+        )
+
+        # Connecting bars
+        draw_rect(pen, b.x1 + b.width / 2, b.y2 - sy, b.x2, b.y2)
+        draw_rect(
+            pen,
+            b.x1 + b.width / 2,
+            ymid,
+            b.x2,
+            ymid + sy,
+        )
+        #
+        # # Right stem
+        draw_parallelogramm(
+            pen,
+            sx,
+            sy,
+            b.x1,
+            0,
+            xb,
+            ymid + sy / 2,
+            direction="top-right",
+        )
