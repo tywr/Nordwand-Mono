6057c34127ce573c58235d0d12caf2f3c2c3c79f
tywr <tanguy.viv@gmail.com>
2026-09-12 20:04:26 +0200

    fix: oslash

--- src/glyphs/latin/oslash.py
+++ src/glyphs/latin/oslash.py
@@ -7,8 +7,8 @@ class OSlashGlyph(RoundLowercaseGlyph):
     name = "oslash"
     unicode = "0xF8"
     offset = 0
-    slash_stroke = 1.2
-    slash_length = 1.4
+    slash_stroke = 1.1
+    slash_length = 1.7
 
     def draw(
         self,
@@ -23,7 +23,7 @@ class OSlashGlyph(RoundLowercaseGlyph):
             overshoot_right=True,
         )
         sx, sy = self.stroke_x_ratio * dc.stroke_x, self.stroke_y_ratio * dc.stroke_y
-        ss = sy * self.slash_length
+        ss = sy * self.slash_stroke
         sl = self.slash_length * b.width
         draw_loop(pen, sx, sy, b.x1, b.y1, b.x2, b.y2, b.hx, b.hy)
         draw_rect(
@@ -32,5 +32,5 @@ class OSlashGlyph(RoundLowercaseGlyph):
             b.ymid - ss / 2,
             b.xmid + sl / 2,
             b.ymid + ss / 2,
-            rotate=45,
+            rotate=55,
         )
